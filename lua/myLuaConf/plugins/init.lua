2a39b50fa6d66854b7560505edc99888dc05160a
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2026-01-23 18:31:47 +0100

    add harpoon config

--- lua/myLuaConf/plugins/init.lua
+++ lua/myLuaConf/plugins/init.lua
@@ -260,4 +260,51 @@ require("lze").load({
       require("todo-comments").setup({})
     end,
   },
+  {
+    "harpoon2",
+    keys = {
+      {
+        "<leader>sa",
+        function()
+          require("harpoon"):list():add()
+        end,
+        desc = "Add to Harpoon",
+      },
+      {
+        "<leader>ss",
+        function()
+          require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())
+        end,
+        desc = "Toggle Harpoon Menu",
+      },
+      {
+        "<leader>1",
+        function()
+          require("harpoon"):list():select(1)
+        end,
+        desc = "Select Harpoon 1",
+      },
+      {
+        "<leader>2",
+        function()
+          require("harpoon"):list():select(2)
+        end,
+        desc = "Select Harpoon 2",
+      },
+      {
+        "<leader>3",
+        function()
+          require("harpoon"):list():select(3)
+        end,
+        desc = "Select Harpoon 3",
+      },
+      {
+        "<leader>4",
+        function()
+          require("harpoon"):list():select(4)
+        end,
+        desc = "Select Harpoon 4",
+      },
+    },
+  },
 })
